fff2d648ada8348fd704247349958a8621cac2b9
Danym2407 <danym2407@gmail.com>
2025-01-28 14:33:04 -0600

    Codigo de Personita donde pasamos datos como nombre y edad

---
+++ Personita/Persona.h
@@ -0,0 +1,16 @@
+//
+//  Persona.h
+//  Personita
+//
+//  Created by Daniela Mendez on 28/01/25.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Persona : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Personita/Persona.m
@@ -0,0 +1,12 @@
+//
+//  Persona.m
+//  Personita
+//
+//  Created by Daniela Mendez on 28/01/25.
+//
+
+#import "Persona.h"
+
+@implementation Persona
+
+@end
